864a06ca09fc2b6892c1264e725d33fc8719a0c8
tarunsavita <tarunsavita08@gmail.com>
2025-07-13 21:35:31 +0530

    frontend complete

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam porro laborum labore impedit reprehenderit atque. Autem perspiciatis minima laboriosam molestias consequuntur fuga tempore distinctio vero. Cupiditate ipsum minus alias et.
+          Repudiandae unde nihil architecto eveniet modi in dolor sed inventore, est qui blanditiis atque nam fuga suscipit dolorem delectus voluptate voluptatum minima, enim, quo perferendis molestiae! Esse nulla optio qui.
+          Officia similique iusto ab quaerat cum corporis pariatur voluptates sunt enim quam ut repellendus consectetur tenetur provident, unde placeat obcaecati sit incidunt quibusdam nostrum labore? A fuga accusamus dolore eum?
+          Itaque nostrum maiores sit ratione dolorum excepturi, voluptatem modi ea enim quo. Minima corrupti illum minus officiis sunt quisquam eos libero nulla dignissimos placeat, voluptates pariatur saepe natus. Laborum, tenetur!
+          Accusamus soluta fuga excepturi, vel assumenda ipsa laudantium eaque ad fugit asperiores earum repudiandae atque sunt aperiam minima illo quidem nobis. Beatae a odit amet suscipit voluptates, aliquid nobis unde.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vero, asperiores tempora quas exercitationem alias accusamus ex soluta expedita vel, voluptatum quidem illo illum laudantium nesciunt reprehenderit voluptates dicta officia.
+            Nihil, aspernatur vitae perspiciatis illum, excepturi qui nisi aliquid similique tenetur accusantium quia recusandae soluta error assumenda ea ullam iure neque quas. Libero quibusdam minima consequatur sed, corporis repellendus obcaecati.
+            Id, nulla? Nulla impedit, debitis minima ratione adipisci aut nemo repellendus accusantium. Molestiae cumque reprehenderit non ipsam ullam officiis accusantium beatae. Eligendi praesentium eveniet et dolores tempore sapiente quaerat. Laudantium!
+          </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
